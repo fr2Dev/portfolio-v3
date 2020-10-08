5d401d6e4702b369c15056226a19b1804884de82
fr2Dev <francois.degrincourt@gmail.com>
2020-10-08 20:40:56 +0200

    [ADD] avatar & devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Avatar from "./avatar";
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <Avatar
+          title="François Degrincourt"
+          titleId="Avatar François Degrincourt"
+        />
       </header>
     </div>
   );
