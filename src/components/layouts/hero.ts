778ae4b32bde1bd3b76dca1eb5d6e999b3086762
fr2Dev <francois.degrincourt@gmail.com>
2020-11-08 19:27:13 +0100

    [UPDATE] Hero style

--- src/components/layouts/hero.ts
+++ src/components/layouts/hero.ts
@@ -9,21 +9,39 @@ export const Hero = styled.section`
 
   & > * + * {
     &:not(h1):not(h2) {
-      margin-top: 3rem;
+      margin-top: 2rem;
+
+      @media only screen and (min-width: 768px) {
+        margin-top: 3rem;
+      }
     }
   }
 
   h1 {
     line-height: 1;
-    margin-top: 2rem;
+    /* margin-top: 1rem;
+
+    @media only screen and (min-width: 768px) {
+      margin-top: 2rem;
+    } */
   }
   h2 {
     line-height: 1;
-    margin-top: 1rem;
+    /* margin-top: 0.5rem;
+
+    @media only screen and (min-width: 768px) {
+      margin-top: 1rem;
+    } */
   }
 
   a {
     place-self: start;
+  }
+
+  @media only screen and (max-width: 767px) {
+    p:first-child {
+      margin-top: 1rem;
+    }
   }
 
   @media only screen and (min-width: 768px) {
