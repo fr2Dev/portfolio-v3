456c46dca65d066a827694e5ab4dc16a78c35e29
fr2Dev <francois.degrincourt@gmail.com>
2020-11-25 23:21:05 +0100

    [ADD] react snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
 import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import App from './App';
 import { SuspenseLoader } from './components';
 import * as serviceWorker from './serviceWorker';
 import './i18n';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const APP = (
   <React.StrictMode>
     <Suspense fallback={<SuspenseLoader />}>
       <App />
     </Suspense>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+if (rootElement?.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
